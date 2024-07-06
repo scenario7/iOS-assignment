baeb627f7f52efea3c46e4a51080cefd5355e741
kevinthomas <kevvin.thomas@gmail.com>
2024-07-06 15:56:34 +0530

    Initial Commit

---
+++ iOS Dev Internship Assigment/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iOS Dev Internship Assigment
+//
+//  Created by Kevin Thomas on 06/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOS Dev Internship Assigment/iOS_Dev_Internship_AssigmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_Dev_Internship_AssigmentApp.swift
+//  iOS Dev Internship Assigment
+//
+//  Created by Kevin Thomas on 06/07/24.
+//
+
+import SwiftUI
+
+@main
+struct iOS_Dev_Internship_AssigmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
